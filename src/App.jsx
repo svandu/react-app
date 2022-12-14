ea4c165906833dab2ddadeb564b6b67eee06ee70
svandu <svandu08@gmail.com>
2022-12-15 02:03:22 +0530

    fetching the data from api

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,18 @@ class App extends Component {
     };
   }
 
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => this.setState(() => {
+      return {monsters: users}
+    },
+    () => {
+      console.log(this.state);
+    }
+    ));
+  }
+
   render() {
     return (
       <div className="App">

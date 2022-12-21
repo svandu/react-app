0ec7605e2010744126186c8600402f4cad6c1704
svandu <svandu08@gmail.com>
2022-12-21 23:58:47 +0530

    event, toLowerCase, setState, map, filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { Component } from 'react';
+import { Component } from "react";
 
-import { useState } from 'react';
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
 
 class App extends Component {
   constructor() {
@@ -11,28 +10,53 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log("constructor");
   }
 
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((users) => this.setState(() => {
-      return {monsters: users}
-    },
-    () => {
-      console.log(this.state);
-    }
-    ));
+  componentDidMount() {
+    console.log("componentDidMount");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
+    console.log("render");
     return (
       <div className="App">
-        {
-          this.state.monsters.map((monster) => {
-            return <div key={monster.id}><h1>{monster.name}</h1></div>;
-          })
-         }
+        <input
+          className="seacrh-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchString = event.target.value.toLocaleLowerCase();
+            //[ { name: 'Leanne'}, {name: 'Yihua'}]
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            })
+          }}
+        />
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
